f95fdd2905f8a7faf865007a701429925e696ead
Jenniffer Macharia <jenn.macharia@gmail.com>
2023-02-24 12:28:03 -0600

    js fuctions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-let apiKey = "7e1abf0a3bocbd20ddt6434fac090605";
-let apiUrl =
-  " https://api.shecodes.io/weather/v1/current?query={Columbia}&key={apiKey}";
+let city = "New York";
+
+let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} :${minutes}`;
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#desription");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+axios.get(apiUrl).then(displayTemperature);

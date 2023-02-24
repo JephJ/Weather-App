bf56aa8cd0606d9d4b8e09e4752fe3733c840f2e
Jenniffer Macharia <jenn.macharia@gmail.com>
2023-02-24 12:47:11 -0600

    iconelement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let city = "New York";
+let city = "Nairobi";
 
 let apiKey = "2ff29bed3181c3526c35cc5408037f85";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -32,12 +32,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 axios.get(apiUrl).then(displayTemperature);

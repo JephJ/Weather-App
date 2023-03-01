432f00cf0fbb8e2a3ed9d6ffe141e8befc251975
Jenniffer Macharia <jenn.macharia@gmail.com>
2023-03-01 17:59:49 -0600

    index

--- src/index.js
+++ src/index.js
@@ -20,26 +20,42 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours} :${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue ", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHtml = `<div class = "row" >`;
-  let days = ["Mon", "Tue ", "Wed", "Thur", "Fri", "Sat"];
+  //let days = ["Sun", "Mon", "Tue ", "Wed", "Thur", "Fri", "Sat"];//
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img src="https://cdn.windy.app/site-storage/posts/February2023/02-weather-symbol-partly-cloudy-windyapp1.jpg" alt=""  
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt=""  
           width = "30"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
